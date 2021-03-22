fe2d53746e25846b06065541e6e6ef1e4e06c2ec
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2021-03-22 08:05:52 +0100

    Adapted script to fetch rexpf material

--- bash/fetch_material.sh
+++ bash/fetch_material.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SCRIPTDIR=/home/quagadmin/source/misctools/
-# DIRTOFETCH=ex/w04
-DIRTOFETCH=sol/w02
+# DIRTOFETCH=ex/w05
+DIRTOFETCH=sol/w03
 TESTSTUDENT=/home/quagadmin/courses/gelasmss2021/students/test_student_usernames_gelasmss2021.txt
 STUDENTUSER=/home/quagadmin/courses/gelasmss2021/students/student_usernames_gelasmss2021.txt
 #' fetch individual directory
