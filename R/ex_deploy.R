f8d9d0afa0cd3ec1245cbb9c92c6b481c46d5318
charlotte-ngs <ngs.charlotte@gmail.com>
2021-04-18 14:34:54 +0200

    Latest version of gel_ex01

--- R/ex_deploy.R
+++ R/ex_deploy.R
@@ -52,12 +52,15 @@ deploy_uni_src <- function(ps_ex_name){
   s_proj_dir <- here::here()
   s_proj_name <- basename(s_proj_dir)
   s_out_dir <- file.path(s_proj_dir, 'docs')
+  # rexpf directory
+  s_rexpf_dir <- file.path(dirname(dirname(s_proj_dir)), 'rexpf', s_proj_name)
   # call the deployment in rteachtools
   rteachtools::deploy_src_to_ex_sol(ps_uni_src_path = file.path(s_proj_dir, 'ex', ps_ex_name, ps_ex_name),
-                                    ps_ex_out_dir = file.path(s_out_dir,"ex"),
-                                    ps_sol_out_dir = file.path(s_out_dir,"sol"),
-                                    ps_nb_src_dir = file.path(s_proj_dir, "nb", ps_ex_name),
-                                    ps_nb_out_dir = file.path(s_out_dir, "nb"))
+                                    ps_ex_out_dir   = file.path(s_out_dir,"ex"),
+                                    ps_sol_out_dir  = file.path(s_out_dir,"sol"),
+                                    ps_nb_src_dir   = file.path(s_proj_dir, "nb", ps_ex_name),
+                                    ps_nb_out_dir   = file.path(s_out_dir, "nb"),
+                                    ps_rexpf_trg    = s_rexpf_dir)
 
   return(invisible(TRUE))
 }
