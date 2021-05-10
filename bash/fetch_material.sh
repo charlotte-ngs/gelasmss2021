ab7b17760adfab0b9fb72a78fa649329fa1836d5
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2021-05-10 07:14:09 +0200

    Distribute exercise and solutions

--- bash/fetch_material.sh
+++ bash/fetch_material.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SCRIPTDIR=/home/quagadmin/source/misctools/
-DIRTOFETCH=ex/gel_ex03
-# DIRTOFETCH=sol/gel_ex02
+# DIRTOFETCH=ex/gel_ex04
+DIRTOFETCH=sol/gel_ex04
 TESTSTUDENT=/home/quagadmin/courses/gelasmss2021/students/test_gel_student_usernames_gelasmss2021.txt
 STUDENTUSER=/home/quagadmin/courses/gelasmss2021/students/gel_student_usernames_gelasmss2021.txt
 #' fetch individual directory
